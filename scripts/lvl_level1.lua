7e965dd191a7f4ccf5a4e647a4e8881ecc91d799
chong900208 <chong900208@gmail.com>
2012-11-18 15:40:14 -0500

    lvl-1

--- scripts/lvl_level1.lua
+++ scripts/lvl_level1.lua
@@ -1,19 +1,86 @@
+counter = 0
+boss = 0
+
 function start()
 	setBGM("battleBGM.ogg")
 	setBG("bg.png")
 end
 
 function update()
-	if elapsedTime() == 120 then
-		addEnemy("enem_sin1", -5, 10)
-		addEnemy("enem_sin1", -5, 15)
-		addEnemy("enem_sin1", -5, 20)
+	if elapsedTime() < 1200 then
+		if counter < 240 then enemypattern1() end
+	elseif elapsedTime() < 2400 then
+		if counter < 240 then enemypattern2() end
+	elseif elapsedTime() < 3600 then
+		if counter < 240 then enemypattern3() end
+	else
+		if boss == 0 then
+			addEnemy("enem_boss1", 300, -150)
+			boss = 1
+		end	
 	end
-	if elapsedTime() == 1800 then
+	
+	if (elapsedTime() % 600) == 0 then
 		addEnemy("enem_special", 400, -10)
 	end
-	if (elapsedTime() == 3600 then
-		addEnemy("enem_boss1", 300, -100)
+	
+	counter = counter + 1
+	if counter > 300 then counter = 1 end
+end
+
+function enemyPattern1()
+	if (elapsedTime() % 20) == 0 then
+		addEnemy("enem_hori1", -10, 100)
+		addEnemy("enem_hori2", 800, 100)
+	end
+	if (elapsedTime() % 40) == 0 then
+		addEnemy("enem_hori1", -10, 150)
+		addEnemy("enem_hori2", 800, 100)
+	end
+	if (elapsedTime() % 60) == 0 then
+		addEnemy("enem_hori1", -10, 200)
+		addEnemy("enem_hori2", 800, 100)
+	end
+end
+
+function enemyPattern3()
+	if (elapsedTime() % 20) == 0 then
+		addEnemy("enem_sine1", -10, 100)
+		addEnemy("enem_sine2", 800, 100)
+		addEnemy("enem_hori1", -10, 0)
+		addEnemy("enem_hori1", -10, 20)
+		addEnemy("enem_hori1", -10, 40)
+	end
+	if (elapsedTime() % 40) == 0 then
+		addEnemy("enem_sine1", -10, 150)
+		addEnemy("enem_sine2", 800, 100)
+		addEnemy("enem_hori1", 800, 0)
+		addEnemy("enem_hori1", 800, 20)
+		addEnemy("enem_hori1", 800, 40)
+	end
+	if (elapsedTime() % 60) == 0 then
+		addEnemy("enem_sine1", -10, 200)
+		addEnemy("enem_sine2", 800, 100)
+		addEnemy("enem_ellipse1", 100, 0)
+		addEnemy("enem_ellopse1", 300, 0)
+		addEnemy("enem_ellipse1", 500, 0)
+	end
+end
+
+function enemyPattern2()
+	if (elapsedTime() % 20) == 0 then
+		addEnemy("enem_cos1", -10, 100)
+		addEnemy("enem_cos2", 800, 100)
+		addEnemy("enem_hori1", -10, 0)
+	end
+	if (elapsedTime() % 40) == 0 then
+		addEnemy("enem_cos1", -10, 150)
+		addEnemy("enem_cos2", 800, 100)
+		addEnemy("enem_hori1", 800, 0)
+	end
+	if (elapsedTime() % 60) == 0 then
+		addEnemy("enem_cos1", -10, 200)
+		addEnemy("enem_cos2", 800, 100)
 	end
 end
 
