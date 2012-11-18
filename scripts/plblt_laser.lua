254425ce376a7df1681ab361de723435fd35759d
Francis <fnha2000@gmail.com>
2012-11-17 23:40:25 -0500

    add weapons

---
+++ scripts/plblt_laser.lua
@@ -0,0 +1,16 @@
+WIDTH = 5
+HEIGHT = 10
+DAMAGE = 5
+posx = 0
+posy = 0
+
+function start(srcx, srcy, tgtx, tgty)
+	posx = srcx-2.5
+	posy = srcy-5
+	init("laser", posx, posy, WIDTH, HEIGHT, DAMAGE, 1, 0)
+end
+
+function update()
+	posy -= 10
+	updatePos(0, 10)
+end
--- scripts/plyr_mouse.lua
+++
@@ -1,3 +0,0 @@
-function start()
-	init("mouse", 2, 375, 500, 50, 50, 100, 5, 3, 1, 0, 5, "camera")
-end
